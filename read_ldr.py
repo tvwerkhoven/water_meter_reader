7375be73afd1f1cdbe8ed784006c54350224b59b
Tim van Werkhoven <timvanwerkhoven@gmail.com>
2018-06-02 16:05:23 +0200

    Add test script for debugging line sensor

---
+++ read_ldr.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+print('starting script')
+import time
+from gpiozero import LineSensor
+from signal import pause
+
+import gpiozero
+print('making sensor')
+#pir = MotionSensor(4)
+sensor = LineSensor(4)
+
+def call1():
+	print("Line detected @ {}".format(time.time()))
+
+
+def call2():
+	print("No line detected @ {}".format(time.time()))
+
+
+print('registering callbacks')
+sensor.when_line = call1
+sensor.when_no_line = call2
+
+print('starting handler')
+pause()
+
